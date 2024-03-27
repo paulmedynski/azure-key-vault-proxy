9103531b449952c4a65a5cbaad49ae39ee8f701d
Paul Medynski <paul.medynski@nuance.com>
2024-03-27 15:04:10 -0300

    Created a minimal dotnet console app to reproduce the proxying issue.

---
+++ KeyVaultProxy.cs
@@ -0,0 +1,408 @@
+// #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*
+// A test program demonstrating that the Azure KeyVault client doesn't
+// respect proxy settings when acquiring authentication tokens.
+//
+// TODO: Link to the GitHub issue.
+
+using Azure.Core;
+using Azure.Core.Diagnostics;
+using Azure.Core.Pipeline;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+
+using System.CommandLine;
+using System.CommandLine.Help;
+using System.CommandLine.Parsing;
+using System.Net;
+
+namespace Medynski;
+
+public sealed class KeyVaultProxy : IDisposable
+{
+  // ==========================================================================
+  // Construction
+
+  // --------------------------------------------------------------------------
+  // Construct with the command line arguments.
+  //
+  // Throws ArgumentException on any errors.
+  //
+  public KeyVaultProxy(string[] args)
+  {
+    // Read the command line arguments into our private members.
+    RootCommand options = new("Azure KeyVault Proxy Test");
+
+    Option<bool> helpOption = new(
+      "--help",
+      () => false,
+      "Emit this help.");
+    options.Add(helpOption);
+
+    Option<LogLevel> logLevelOption = new(
+      "--log-level",
+      () => LogLevel.Information,
+      "Set the log level as a number.");
+    logLevelOption.AddAlias("-l");
+    options.Add(logLevelOption);
+
+    Option<string> kvUrlOption = new(
+      "--url",
+      "The URL of the target Azure Key Vault.");
+    kvUrlOption.AddAlias("-u");
+    options.Add(kvUrlOption);
+
+    Option<string> tenantIdOption = new(
+      "--tenant-id",
+      "The Azure Key Vault tenant ID.");
+    tenantIdOption.AddAlias("-t");
+    options.Add(tenantIdOption);
+
+    Option<string> appIdOption = new(
+      "--app-id",
+      "The Azure Key Vault application ID.");
+    appIdOption.AddAlias("-a");
+    options.Add(appIdOption);
+
+    Option<string> secretOption = new(
+      "--secret",
+      "The secret to use for Azure Key Vault authentication.  Specify either "
+      + "this or --cert-path, but not both.");
+    secretOption.AddAlias("-s");
+    options.Add(secretOption);
+
+    Option<string> certPathOption = new(
+      "--cert-path",
+      "The path to the certificate file to use for Azure Key Vault "
+      + "authentication.  Specify either this or --secret, but not both.");
+    certPathOption.AddAlias("-c");
+    options.Add(certPathOption);
+
+    Option<string> keyOption = new(
+      "--key",
+      "The key of the secret to obtain.");
+    keyOption.AddAlias("-k");
+    options.Add(keyOption);
+
+    Option<string?> proxyOption = new(
+      "--proxy",
+      () => null,
+      "The proxy host:port; omit for direct connection.");
+    proxyOption.AddAlias("-p");
+    options.Add(proxyOption);
+
+    Option<bool> globalProxyOption = new(
+      "--global-proxy",
+      () => false,
+      "If --proxy is specified, use it as the global HttpClient.DefaultProxy "
+      + "instead of the per-instance HttpClient proxy.");
+    options.Add(globalProxyOption);
+
+    var parseResult = options.Parse(args);
+
+    if (parseResult.GetValueForOption(helpOption))
+    {
+        new HelpBuilder(LocalizationResources.Instance)
+            .Write(options, Console.Out);
+        throw new ArgumentException("Help requested");
+    }
+
+    _logLevel = parseResult.GetValueForOption(logLevelOption);
+
+    try
+    {
+      var kvUrlString = GetRequiredString(parseResult, kvUrlOption);
+      _kvUrl = new Uri(kvUrlString);
+    }
+    catch (UriFormatException ex)
+    {
+      throw new ArgumentException(
+        $"Error: --url must be a valid URL: {ex.Message}");
+    }
+
+    _tenantId = GetRequiredString(parseResult, tenantIdOption);
+    _appId = GetRequiredString(parseResult, appIdOption);
+
+    _secret = parseResult.GetValueForOption(secretOption);
+    _certPath = parseResult.GetValueForOption(certPathOption);
+    
+    if ((_secret is null && _certPath is null)
+        || (_secret is not null && _certPath is not null))
+    {
+      throw new ArgumentException(
+        "Error: Specify either --secret or --cert-path, but not both.");
+    }
+
+    _key = GetRequiredString(parseResult, keyOption);
+    
+    var proxyHostPort = parseResult.GetValueForOption(proxyOption);
+    if (proxyHostPort is not null)
+    {
+      var proxyParts = proxyHostPort.Split(':');
+      if (proxyParts.Length != 2)
+      {
+        throw new ArgumentException(
+          "Error: --proxy must be in the form host:port.");
+      }
+  
+      _proxyHost = proxyParts[0];
+      
+      try
+      {
+        _proxyPort = UInt16.Parse(proxyParts[1]);
+      }
+      catch (Exception ex)
+      when (ex is FormatException || ex is OverflowException)
+      {
+        throw new ArgumentException(
+          $"Error: --proxy port must be a valid uint16: {ex.Message}");
+      }
+    }
+
+    _globalProxy = parseResult.GetValueForOption(globalProxyOption);
+  }
+  
+  // --------------------------------------------------------------------------
+  // Dispose of our private members.
+  //
+  public void Dispose()
+  {
+    _httpClient?.Dispose();
+    _proxyRestorer?.Dispose();
+  }
+
+  // ==========================================================================
+  // Interface
+
+  // --------------------------------------------------------------------------
+  public int Run()
+  { 
+    // Log to the console.
+    using ILoggerFactory logFactory =
+      LoggerFactory.Create(
+        (ILoggingBuilder builder) =>
+        {
+          builder
+          .SetMinimumLevel(_logLevel)
+          // This doesn't appear to log HttpClient messages unfortunately.
+          .AddFilter("System.Net.Http", LogLevel.Debug)
+          .AddSimpleConsole(
+            (SimpleConsoleFormatterOptions options) =>
+            {
+              options.IncludeScopes = true;
+              options.SingleLine = true;
+              options.TimestampFormat = "HH:mm:ss.fff ";
+              options.ColorBehavior = LoggerColorBehavior.Enabled;
+            });
+        });
+    
+    var log = logFactory.CreateLogger(nameof(KeyVaultProxy));
+    // Send Azure debugging to the log.
+    using AzureEventSourceListener _azureLogger = new(
+      (args, message) =>
+      {
+        using var scope = log.BeginScope("Azure");
+        
+        // Core events are logged at Debug.
+        if (args.EventSource.Name == "Azure-Core")
+        {
+          log.LogDebug(message);
+        }
+        // Everything else is logged at Trace.
+        else
+        {
+          log.LogTrace(message);
+        }
+      },
+      // Ask for all events of all levels.
+      level: System.Diagnostics.Tracing.EventLevel.LogAlways);
+    
+    // We may need to specify a custom HTTP handler so we will need some
+    // SecretClient options.
+    SecretClientOptions secretOptions = new();
+
+    // Are we using a proxy?
+    if (_proxyHost is not null)
+    {
+      // Yes, so set it up with the given host/port.
+      var proxy = new WebProxy(_proxyHost, _proxyPort)
+      {
+          BypassProxyOnLocal = false,
+          UseDefaultCredentials = false
+      };
+
+      // Is it global?
+      if (_globalProxy)
+      {
+        // Ensure we restore the default proxy regardless of how we exit.
+        _proxyRestorer = new();
+
+        // Assign our proxy to HttpClient's default proxy so all HttpClient
+        // instances without their own explicit proxy will use this one.
+        //
+        // This seems to be the only way to have SecretClient use the proxy
+        // for token acquisition requests.
+        //
+        HttpClient.DefaultProxy = proxy;
+      }
+      // No, so we need to use a custom HttpClient.
+      else
+      {
+        // Create HttpClient that uses our proxy.
+        _httpClient = new(
+          new HttpClientHandler
+          {
+            Proxy = proxy,
+            UseProxy = true
+          });
+        
+        // Tell SecretClient to use our HttpClient, and thus our proxy.
+        //
+        // Unfortunately, SecretClient seems to only use this HttpClient for
+        // KeyVault requests.  It uses a different HttpClient for token
+        // acquisition, thus ignoring our proxy for those requests.
+        //
+        secretOptions.Transport = new HttpClientTransport(_httpClient);
+      }
+    }
+
+    TokenCredential credential =
+        _secret is not null
+        ? new ClientSecretCredential(_tenantId, _appId, _secret)
+        : new ClientCertificateCredential(_tenantId, _appId, _certPath);
+    
+    log.LogInformation(
+      $"Creating SecretClient for vault URL={_kvUrl} tenantId={_tenantId} "
+      + $"appId={_appId}...");
+
+    log.LogInformation(
+      $"Using auth type="
+      + (_secret is not null ? "secret" : $"cert ({_certPath})"));
+    
+    if (_proxyHost is not null)
+    {
+      log.LogInformation(
+        $"Using proxy {_proxyHost}:{_proxyPort} "
+        + (_globalProxy ? " (global)" : ""));
+    }
+      
+    SecretClient secrets = new(_kvUrl, credential, secretOptions);
+
+    log.LogInformation($"Getting secret for key={_key}...");
+    
+    try
+    {
+      var kvSecret = secrets.GetSecret(_key);
+      var kvValue = kvSecret.Value;
+      log.LogInformation(
+        $"Secret for key={_key} is: id={kvValue.Id} name={kvValue.Name} "
+        + $"value={kvValue.Value}");
+    }
+    catch (Exception ex)
+    {
+      log.LogError($"Failed to fetch secret for key={_key}: {ex.Message}");
+      return 1;
+    }
+
+    return 0;
+  }
+
+  // ==========================================================================
+  // Helpers
+
+  // --------------------------------------------------------------------------
+  // An RAII helper to restore the HttpClient's default proxy.
+  //
+  class ProxyRestorer : IDisposable
+  {
+    public ProxyRestorer()
+    {
+      _oldProxy = HttpClient.DefaultProxy;
+    }
+
+    public void Dispose()
+    {
+      HttpClient.DefaultProxy = _oldProxy;
+    }
+
+    private readonly IWebProxy _oldProxy;
+  }
+
+  // --------------------------------------------------------------------------
+  // Get the value of a required option.
+  //
+  // Throws ArgumentException if the option is not present.
+  //
+  private T GetRequired<T>(
+    ParseResult result,
+    Option<T> option)
+  where T : notnull
+  {
+    var optionValue = result.GetValueForOption(option);
+
+    if (optionValue is null)
+    {
+      throw new ArgumentException($"Error: {option.Name} is required.");
+    }
+
+    return optionValue;
+  }
+
+  // --------------------------------------------------------------------------
+  // Get the value of a required string option.
+  //
+  // Throws ArgumentException if the option is not present or its value is the
+  // empty string.
+  //
+  private string GetRequiredString(
+    ParseResult result,
+    Option<string> option)
+  {
+    var value = GetRequired(result, option);
+    if (value.Length == 0)
+    {
+      throw new ArgumentException($"Error: {option.Name} must not be empty.");
+    }
+    return value;
+  }
+
+  // --------------------------------------------------------------------------
+  // The main entry point, called by the .NET runtime.
+  //
+  public static int Main(string[] args)
+  {
+    try
+    {
+      using KeyVaultProxy app = new(args);
+      return app.Run();
+    }
+    catch (ArgumentException ex)
+    {
+      Console.Error.WriteLine($"Error: {ex.Message}");
+      return 1;
+    }
+  }
+
+  // ==========================================================================
+  // Private Members
+
+  // Command line options.
+  private readonly LogLevel _logLevel;
+  private readonly Uri _kvUrl;
+  private readonly string _tenantId;
+  private readonly string _appId;
+  private readonly string? _secret;
+  private readonly string? _certPath;
+  private readonly string _key;
+  private readonly string? _proxyHost;
+  private readonly ushort _proxyPort;
+  private readonly bool _globalProxy;
+
+  // Proxy management.
+  private ProxyRestorer? _proxyRestorer;
+  private HttpClient? _httpClient;
+}
+
+// #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*
--- README.md
+++ README.md
@@ -1,2 +1,77 @@
 # azure-key-vault-proxy
-Minimal project to reproduce the issue that Azure SecretClient doesn't obey proxy settings for login token acquisition.
+Minimal project to reproduce the issue that Azure SecretClient doesn't obey
+proxy settings for login token acquisition.
+
+We can configure the SecretClientOptions.Transport with a suitable proxy, and
+requests to the Azure KeyVault obey this proxy.  However, requests from within
+SecretClient to obtain login tokens _do not_ obey the proxy, and thus they fail
+to complete.  If a proxy is set via SecretClientOptions.Transport, it should be
+used for all requests made by SecretClient or any libs/functions that it uses.
+
+Use this minimal dotnet console app to reproduce the issue.  Run with --help for
+options related to Azure Key Vault and whether or not a proxy is used.
+
+### All egress allowed, no proxy specified:
+```
+$ dotnet run -- -u https://<keyvault>/ -t <tenantId> -a <appId> -c <cert.pem> -k my-secret-name
+
+11:54:30.338 info: KeyVaultProxy[0] Creating SecretClient for vault URL=https://<keyvault>/ tenantId=<tenantId> appId=<appId>...
+11:54:30.351 info: KeyVaultProxy[0] Using auth type=cert (cert.pem)
+11:54:30.353 info: KeyVaultProxy[0] Getting secret for key=my-secret-name...
+11:54:31.606 info: KeyVaultProxy[0] Secret for key=my-secret-name is: id=https://<keyvault>/secrets/my-secret-name/945c0e7946314f90bb3d5e6f74e1d120 name=my-secret-name value=shhh_secret
+```
+
+### No egress allowed, no proxy specified:
+
+Configure an HTTPS proxy on localhost:8888, for example
+[tinyproxy](https://tinyproxy.github.io/).
+
+Configure iptables to reject all outbound traffic except from the proxy:
+```
+# iptables -F OUTPUT
+# iptables -A OUTPUT -j ACCEPT -m owner --uid-owner tinyproxy
+# iptables -A OUTPUT -j ACCEPT -o lo
+# iptables -A OUTPUT -j REJECT
+```
+
+```
+$ dotnet run -- -u https://<keyvault>/ -t <tenantId> -a <appId> -c <cert.pem> -k my-secret-name
+
+14:34:13.245 info: KeyVaultProxy[0] Creating SecretClient for vault URL=https://<keyvault>/ tenantId=<tenantId> appId=<appId>...
+14:34:13.258 info: KeyVaultProxy[0] Using auth type=cert (cert.pem)
+14:34:13.260 info: KeyVaultProxy[0] Getting secret for key=my-secret-name...
+14:34:19.074 fail: KeyVaultProxy[0] Failed to fetch secret for key=my-secret-name: Retry failed after 4 tries. Retry settings can be adjusted in ClientOptions.Retry or by configuring a custom retry policy in ClientOptions.RetryPolicy. (Connection refused (<keyvault>:443)) (Connection refused (<keyvault>:443)) (Connection refused (<keyvault>:443)) (Connection refused (<keyvault>:443))
+```
+
+Note that the initial connection to the Key Vault is refused, as expected.
+
+### No egress allowed, SecretClient proxy specified:
+```
+$ dotnet run -- -u https://<keyvault>/ -t <tenantId> -a <appId> -c <cert.pem> -k my-secret-name -p localhost:8888
+
+14:36:14.837 info: KeyVaultProxy[0] Creating SecretClient for vault URL=https://<keyvault>/ tenantId=<tenantId> appId=<appId>...
+14:36:14.848 info: KeyVaultProxy[0] Using auth type=cert (cert.pem)
+14:36:14.848 info: KeyVaultProxy[0] Using proxy localhost:8888
+14:36:14.850 info: KeyVaultProxy[0] Getting secret for key=my-secret-name...
+14:36:21.633 fail: KeyVaultProxy[0] Failed to fetch secret for key=my-secret-name: ClientCertificateCredential authentication failed: Retry failed after 4 tries. Retry settings can be adjusted in ClientOptions.Retry or by configuring a custom retry policy in ClientOptions.RetryPolicy. (Network is unreachable (login.microsoftonline.com:443)) (Network is unreachable (login.microsoftonline.com:443)) (Network is unreachable (login.microsoftonline.com:443)) (Network is unreachable (login.microsoftonline.com:443)) See the troubleshooting guide for more information. https://aka.ms/azsdk/net/identity/clientcertificatecredential/troubleshoot
+```
+
+Now we see that the login token requests are failing, because they aren't using
+the proxy!  This can be corroborated by inspecting the tinyproxy logs and noting
+that only the initial Key Vault was tunneled through the proxy.  The login token
+requests did not pass throug the proxy.
+
+### No egress allowed, HttpClient.DefaultProxy specified:
+```
+$ dotnet run -- -u https://<keyvault>/ -t <tenantId> -a <appId> -c <cert.pem> -k my-secret-name -p localhost:8888 --global-proxy
+
+14:37:50.710 info: KeyVaultProxy[0] Creating SecretClient for vault URL=https://<keyvault>/ tenantId=<tenantId> appId=<appId>...
+14:37:50.720 info: KeyVaultProxy[0] Using auth type=cert (cert.pem)
+14:37:50.721 info: KeyVaultProxy[0] Using proxy localhost:8888  (global)
+14:37:50.722 info: KeyVaultProxy[0] Getting secret for key=my-secret-name...
+14:37:52.433 info: KeyVaultProxy[0] Secret for key=my-secret-name is: id=https://<keyvault>/secrets/my-secret-name/945c0e7946314f90bb3d5e6f74e1d120 name=my-secret-name value=shhh_secret
+```
+
+Everything works when the global HttpClient.DefaultProxy is specified.  This
+isn't a suitable solution though, since other HTTP connections within the app
+may rely on a default proxy specified by environment variables for example.
